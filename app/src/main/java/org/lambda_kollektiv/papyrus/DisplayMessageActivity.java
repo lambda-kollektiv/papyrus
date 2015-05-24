887e0e0873226cf694390252424472de64c12088
kordano <konrad.kuehne@rocketmail.com>
2015-05-24 17:42:32 +0200

    add second activity

---
+++ app/src/main/java/org/lambda_kollektiv/papyrus/DisplayMessageActivity.java
@@ -0,0 +1,42 @@
+package org.lambda_kollektiv.papyrus;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ListView.EXTRA_MESSAGE);
+
+        // Create text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
